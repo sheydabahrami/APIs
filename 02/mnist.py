bd856210b4d36447670ceacf8e907e115c577eed
Sheyda Bahrami <seydabahrami@gmail.com>
2021-11-26 13:22:31 +0300

    Create mnist.py

---
+++ 02/mnist.py
@@ -0,0 +1,121 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+import os
+import json
+import argparse
+from tensorflow.python.platform import tf_logging
+import logging as lg
+import sys as sys
+
+
+def cnn_mode_fn(features, labels, mode):
+input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+
+conv1 = tf.layers.conv2d(
+inputs=input_layer,
+filters = 32,
+kernel_size=[5, 5],
+padding = 'same',
+activation=tf.nn.relu)
+
+pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+conv2 = tf.layers.conv2d(
+inputs=pool1,
+filters = 64,
+kernel_size=[5, 5],
+padding = 'same',
+activation=tf.nn.relu)
+
+pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+
+dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+dropout = tf.layers.dropout(
+inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+logits = tf.layers.dense(inputs=dropout, units=10)
+
+predictions = {
+"classes":tf.argmax(input=logits, axis=1),
+"probabilities":tf.nn.softmax(logits, name="softmax_tensor")
+}
+
+if mode == tf.estimator.ModeKeys.PREDICT:
+return tf.estimator.EstimatorSpec(mode=mode, predictions = predictions)
+
+loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+if mode == tf.estimator.ModeKeys.TRAIN:
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+train_op = optimizer.minimize(
+loss=loss,
+global_step = tf.train.get_global_step())
+
+return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+eval_metric_ops = {
+"accuracy":tf.metrics.accuracy(
+labels=labels, predictions = predictions["classes"])}
+return tf.estimator.EstimatorSpec(
+mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+def _load_training_data(base_dir):
+x_train = np.load(os.path.join(base_dir, 'train_data.npy'))
+y_train = np.load(os.path.join(base_dir, 'train_labels.npy'))
+return x_train, y_train
+
+def _load_testing_data(base_dir):
+x_test = np.load(os.path.join(base_dir, 'eval_data.npy'))
+y_test = np.load(os.path.join(base_dir, 'eval_labels.npy'))
+return x_test, y_test
+
+def _parse_args():
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_dir', type=str)
+parser.add_argument('--sm-model-dir', type=str, default=os.environ.get("SM_MODEL_DIR"))
+parser.add_argument('--train', type=str, default=os.environ.get("SM_CHANNEL_TRAINING"))
+parser.add_argument('--hosts', type=list, default=json.loads(os.environ.get("SM_HOSTS")))
+parser.add_argument('--current-host', type=str, default=os.environ.get("SM_CURRENT_HOST"))
+
+return parser.parse_known_args()
+def serving_input_fn():
+inputs = {'x': tf.placeholder(tf.float32, [None, 784])}
+return tf.estimator.export.ServingInputReceiver(inputs,inputs)
+
+if name == 'main':
+args, unknown = _parse_args()
+
+train_data, train_labels = _load_training_data(args.train)
+eval_data, eval_labels = _load_testing_data(args.train)
+
+mnist_classifier = tf.estimator.Estimator(
+model_fn = cnn_mode_fn, model_dir=args.model_dir)
+
+tensors_to_log = {"probabilities" : "softmax_tensor"}
+logging_hook = tf.train.LoggingTensorHook(
+tensors=tensors_to_log, every_n_iter=50)
+
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+x={"x":train_data},
+y=train_labels,
+batch_size=100,
+num_epochs=None,
+shuffle=True)
+
+eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+x={"x":eval_data},
+y=eval_labels,
+num_epochs=1,
+shuffle=false)
+
+train_spec = tf.estimator.TrainSpec(train_input_fn, max_steps=20000)
+eval_spec = tf.estimator.EvalSpec(eval_input_fn)
+tf.estimator.train_and_evaluate(mnist_classifier, train_spec, eval_spec)
+
+if args.current_host == args.hosts[0]:
+mnist_classifier.export_savedmodel(args.sm_model_dir, serving_input_fn)
